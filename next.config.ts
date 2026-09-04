06c455733e47c05bd43b28813af9c586b7ad6182
Mareko James <mmj342@nyu.edu>
2026-09-03 20:31:58 -0400

    fix image grabbing

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "**.public.blob.vercel-storage.com",
-        pathname: "/pics/**",
+        pathname: "/**",
       },
     ],
   },
